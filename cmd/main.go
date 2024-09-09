ff4d25847c938a26fcbd712a51f4fb167a85beea
Daniil Bykov <daniil0604bykiv@gmail.com>
2024-09-09 19:01:31 +0300

    реализована холостая логика

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net"
 
-	"github.com/fatih/color"
+	desc "github.com/Dnlbb/auth/pkg/auth"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
+type server struct {
+	desc.UnimplementedAuthServer
+}
+
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatal("failed to listen: 50051 ")
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterAuthServer(s, &server{})
+
+	log.Printf("server listening at #{lis.Addr()}")
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatal("failed to serve: #{err}")
+	}
+
 }
