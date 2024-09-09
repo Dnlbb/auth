fefe98cf13d0e17f586794bf2f91b4bb3ead62ce
Daniil Bykov <daniil0604bykiv@gmail.com>
2024-09-09 19:27:35 +0300

    реализована холостая логика

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,43 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 
 	desc "github.com/Dnlbb/auth/pkg/auth"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 type server struct {
 	desc.UnimplementedAuthServer
 }
 
+func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	log.Printf("user id:%d", req.GetId())
+	return &desc.GetResponse{}, nil
+}
+
+func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("User #+%v\n", req.GetUser())
+	log.Printf("Password: %s", req.Password)
+	log.Printf("Password confirm: %s", req.PasswordConfirm)
+	return &desc.CreateResponse{}, nil
+}
+
+func (s *server) Update(ctx context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
+	log.Printf("User id: %d", req.GetId())
+	log.Printf("Username: %s", req.Name.Value)
+	log.Printf("Email: %s", req.Email.Value)
+	return nil, nil
+}
+
+func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("User id: %d", req.GetId())
+	return nil, nil
+}
 func main() {
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
