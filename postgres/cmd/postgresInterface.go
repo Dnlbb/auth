dc2d04c2702934d6ea3db585e77611a14db1e3dd
Daniil Bykov <daniil0604bykiv@gmail.com>
2024-10-14 19:46:37 +0300

    models

--- postgres/cmd/postgresInterface.go
+++ postgres/cmd/postgresInterface.go
@@ -1,11 +1,5 @@
 package dao
 
-import (
-	"time"
-
-	auth "github.com/Dnlbb/auth/pkg/auth_v1"
-)
-
 // PostgresInterface Далее будет заменен для гибкой работы с бд
 type PostgresInterface interface {
 	Save(user User) error
@@ -13,30 +7,3 @@ type PostgresInterface interface {
 	Delete(id DeleteID) error
 	GetUser(params GetUserParams) (*User, error)
 }
-type (
-	// User for db
-	User struct {
-		ID        int64
-		Name      string
-		Email     string
-		Password  string
-		Role      auth.Role
-		CreatedAt time.Time
-		UpdatedAt time.Time
-	}
-	// UpdateUser for db
-	UpdateUser struct {
-		ID       int64
-		Name     string
-		Email    string
-		Password string
-		Role     auth.Role
-	}
-	// DeleteID for db
-	DeleteID int64
-	// GetUserParams for db
-	GetUserParams struct {
-		ID       *int64
-		Username *string
-	}
-)
